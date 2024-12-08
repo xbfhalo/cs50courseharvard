5d0d77e3928560b21a53be26849317acc6ba959b
Thomas (xbfHalo) MO <thomasxbf.creator@gmail.com>
2024-12-08 19:08:41 +0000

    example

--- Week_1_C/app.c
+++ Week_1_C/app.c
@@ -6,4 +6,5 @@ int main(void)
     string first_name = get_string("What is your first name?: ");
     string last_name = get_string("What is your last name?: ");
     printf("Hello, %s %s\n", first_name, last_name);
+    
 }
